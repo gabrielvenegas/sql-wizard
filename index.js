50d487daf1c87cf1e5394d5b1553a5c3b5b87f9e
Gabriel Enrique <ged.venegas@gmail.com>
2023-11-16 15:04:01 -0300

    mssql supporte

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { createConnection } from "mysql";
 import fs from "fs";
 import inquirer from "inquirer";
 import pg from "pg";
+import sql from "mssql";
 
 async function getDatabaseConfig() {
   const questions = [
@@ -11,13 +12,18 @@ async function getDatabaseConfig() {
       type: "list",
       name: "type",
       message: "Select your database type:",
-      choices: ["mysql", "pg"],
+      choices: ["mysql", "postgres", "sql server"],
     },
     {
       type: "input",
       name: "host",
       message: "Enter your database host:",
       default: "localhost",
+    },
+    {
+      type: "input",
+      name: "database",
+      message: "Enter your database name:",
     },
     {
       type: "input",
@@ -31,16 +37,13 @@ async function getDatabaseConfig() {
     },
     {
       type: "input",
-      name: "database",
-      message: "Enter your database name:",
-    },
-    {
-      type: "input",
       name: "port",
       message: "Enter your database port:",
       default: (response) => {
-        if (response.type === "pg") {
+        if (response.type === "postgres") {
           return 5432;
+        } else if (response.type === "sql server") {
+          return 1433;
         } else {
           return 3306;
         }
@@ -165,15 +168,80 @@ async function getPostgresMetadata(config) {
   });
 }
 
-async function getMSSQLMetadata(config) {}
+async function getMSSQLMetadata(config) {
+  try {
+    // make sure that any items are correctly URL encoded in the connection string
+    const connection = await sql.connect({
+      server: config.host,
+      user: config.user,
+      password: config.password,
+      database: config.database,
+      port: config.port,
+      options: {
+        trustedConnection: true,
+        encrypt: true,
+        enableArithAbort: true,
+        trustServerCertificate: true,
+      },
+    });
+
+    const result = await sql.query`
+      SELECT 
+        t.name AS TableName,
+        c.name AS ColumnName,
+        ty.name AS DataType,
+        fk.name AS ForeignKeyConstraintName,
+        pt.name AS ParentTableName,
+        pc.name AS ParentColumnName,
+        ck.name AS CheckConstraintName,
+        ck.definition AS CheckConstraintDefinition,
+        pk.name AS PrimaryKeyConstraintName
+      FROM 
+          sys.tables t
+      INNER JOIN 
+          sys.columns c ON t.object_id = c.object_id
+      INNER JOIN 
+          sys.types ty ON c.system_type_id = ty.system_type_id
+      LEFT JOIN 
+          sys.foreign_key_columns fkc ON fkc.parent_object_id = t.object_id AND fkc.parent_column_id = c.column_id
+      LEFT JOIN 
+          sys.foreign_keys fk ON fk.object_id = fkc.constraint_object_id
+      LEFT JOIN 
+          sys.tables pt ON fk.referenced_object_id = pt.object_id
+      LEFT JOIN 
+          sys.columns pc ON fkc.referenced_column_id = pc.column_id AND pc.object_id = pt.object_id
+      LEFT JOIN 
+          sys.check_constraints ck ON ck.parent_object_id = t.object_id
+      LEFT JOIN 
+          sys.index_columns ik ON ik.object_id = t.object_id AND ik.column_id = c.column_id
+      LEFT JOIN 
+          sys.key_constraints pk ON pk.parent_object_id = t.object_id AND pk.type = 'PK' AND ik.index_id = pk.unique_index_id
+      ORDER BY 
+          t.name, c.column_id;
+    `;
+
+    fs.writeFileSync("metadata.json", JSON.stringify(normalizedStructure));
+
+    console.log("✅ Arquivo metadata.json salvo com sucesso!");
+
+    console.log(
+      "🪄  Agora é só importar no https://chat.openai.com/g/g-b6NBRSd47-sql-wizard e começar a gerar suas queries!"
+    );
+  } catch (err) {
+    console.log(err);
+    // ... error checks
+  }
+}
 
 async function main() {
   const config = await getDatabaseConfig();
 
   if (config.type === "mysql") {
     await getMySQLMetadata(config);
-  } else if (config.type === "pg") {
+  } else if (config.type === "postgres") {
     await getPostgresMetadata(config);
+  } else if (config.type === "sql server") {
+    await getMSSQLMetadata(config);
   }
 }
 
